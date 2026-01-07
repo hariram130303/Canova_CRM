e5e601115f8f5ad50a3053fd90bc5cab1307e9ca
hariram130303 <tmhariram@gmail.com>
2026-01-07 20:01:01 +0530

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://backend-canova-crm.onrender.com",
+  baseURL: "https://backend-canova-crm.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
